4e3b50a0f8e591d1754dbc1c619a261529776954
Aref Mirhosseini <code@arefmirhosseini.com>
2016-12-04 21:20:58 +0330

    change configs for staging server

--- server/lib/utils/lib/file-system/core.js
+++ server/lib/utils/lib/file-system/core.js
@@ -37,12 +37,10 @@ exports.ensurePathExists = function (path, options) {
     return new Promise(function (resolve, reject) {
         function handleAccess(err) {
             if (err) {
-                console.log("handleAccess-1", err);
                 if (err.path) {
                     mkdirPath(err.path, mode).then(() => {
                         resolve(err.path);
-                    }).catch((err2) => {
-                        console.log("handleAccess-2", err2);
+                    }).catch(() => {
                         reject(FsErrors.NoAccess(err.path));
                     });
                 }
