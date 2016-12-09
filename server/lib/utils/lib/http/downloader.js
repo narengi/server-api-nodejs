84bd78413529d69c1803168eefe538fc5c1113e5
Aref Mirhosseini <code@arefmirhosseini.com>
2016-12-09 14:39:43 +0330

    change image render method in narengi utils

--- server/lib/utils/lib/http/downloader.js
+++ server/lib/utils/lib/http/downloader.js
@@ -20,15 +20,6 @@ exports.download = function(res, filename, filepath){
 
     // res.setHeader('Content-disposition', 'attachment; filename=' + filename);
     res.setHeader('Content-type', mimetype);
-
-    switch (mimetype) {
-    	case 'image/jpeg': 
-    		let fileToLoad = nodeFs.readFileSync(filepath);
-    		res.writeHead(200, {'Content-Type':  mimetype });
-    		res.end(fileToLoad, 'binary');
-    	break;
-    	default:
-		    var filestream = nodeFs.createReadStream(filepath);
-		    filestream.pipe(res);
-    }
+    var filestream = nodeFs.createReadStream(filepath);
+    filestream.pipe(res);
 };
