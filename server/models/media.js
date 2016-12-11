bb390e8fdf8b8e00da9323ec51ec18305b758ded
Aref Mirhosseini <code@arefmirhosseini.com>
2016-12-11 20:18:40 +0330

    add media model

--- server/models/media.js
+++ server/models/media.js
@@ -1,14 +1,20 @@
 'use strict'
 
 const loopback = require('loopback');
-const configs = serverRequire('server').settings;
+const app = serverRequire('server');
+const configs = app.settings;
 const MainHandler = require('../Handlers/Main');
+const Http = require('narengi-utils').Http;
+const lwip = require('lwip');
+const async = require('async');
+const crypto = require('crypto');
+const _ = require('lodash');
 
 class Medias extends MainHandler {
 
     constructor(Media) {
         super(Media)
-            // Register Methods
+        // Register Methods
         this.upload()
     }
 
@@ -17,49 +23,111 @@ class Medias extends MainHandler {
         let Settings = {
             name: 'upload',
             description: 'upload new medias',
-            path: '/upload',
+            path: '/upload/:section?',
             method: 'post',
             status: 201,
             accepts: [{
-                arg: 'data',
+                arg: 'req',
                 type: 'object',
                 http: {
-                    source: 'body'
+                    source: 'req'
                 }
             }],
             returns: {
-                arg: 'fileObject',
-                type: 'object',
-                root: true
+                arg: 'result',
+                type: 'object'
             }
         }
 
-        this.registerMethod(Settings, (data, cb) => {
+        this.registerMethod(Settings, (req, cb) => {
 
-        	console.log(loopback.StorageService);
-        	// required options: maxFileSize
+            const Storage = app.models.Storage;
+            let container = req.params.section && req.params.section.trim().length ? `${req.params.section.trim()}` : `cached`;
+            let contcfg = _.has(configs, container) ? configs[container].picture : {};
+            let ctx = loopback.getCurrentContext();
+            let currentUser = ctx.get('currentUser');
 
-            // let ctx = loopback.getCurrentContext();
-            // let currentUser = ctx.get('currentUser');
-            // if (!currentUser) return cb(Security.Errors.NotAuthorized());
+            // VALIDATE CONFIGS
+            switch (true) {
+            	case !Object.keys(contcfg).length:
+            	case !_.has(contcfg, 'dirName') || !contcfg.dirName.trim().length:
+            	case !_.has(contcfg, 'maxSize') || typeof contcfg.maxSize !== "number":
+            	case !_.has(contcfg, 'key') || !contcfg.key.trim().length:
+            		cb('section configs is not valid');
+            	break;
+            }
 
-            // let config = configs.userProfile.picture;
-            // let uploadOptions = {
-            //     owner_id: currentUser.id,
-            //     destDir: "",
-            //     fieldName: "",
-            //     maxSize: "",
-            //     hash: "",
-            //     styles: ""
-            // };
+            async.waterfall([
+            	(callback) => {
+            		// CHECK IF CONTAINER IS EXISTS
+            		Storage.getContainer(contcfg.dirName, (err) => callback(null, err ? err.code : 'OK'))
+            	},
+            	(cont, callback) => {
+            		// CREATE CONTAINER IF IT IS NOT EXISTS
+            		switch (cont) {
+            			case 'OK': callback(null)
+            			case 'ENOENT':
+            				Storage.createContainer({ name: contcfg.dirName }, () => callback(null))
+            			break;
+            		}
+            	},
+            	(callback) => {
+            		// GET FILE FROM REQUEST
+            		Http.Uploader.mediaUpload(req)
+            			.then((file) => callback(null, file))
+            			.catch((err) => callback(err))
+            	},
+            	(file, callback) => {
+            		// VALIDATE FILE BASE ON CONTAINER CONFIGS
+            		let isValid = true;
+            		let errMsg = null;
 
-            // options["destDir"] = `${__dirname}/../../${config.root}/${config.dirName}/${options.uid}`;
-            // options["fieldName"] = config.fieldName;
-            // options["maxSize"] = config.maxSize;
-            // options["hash"] = true;
-            // options["styles"] = underscore.extend({ original: "original" }, config.styles);
+            		isValid = isValid && file.type.substring(0, file.type.indexOf('/')) === "image";
+            		if (!isValid && !errMsg) errMsg = "error-file-type";
 
-            cb(null, "OK")
+            		isValid = isValid && file.size <= contcfg.maxSize;
+            		if (!isValid && !errMsg) errMsg = "error-file-size";
+
+            		callback(errMsg, isValid ? file : null);
+
+            	},
+            	(file, callback) => {
+            		// CREATE IMAGE OBJECT FROM FILE
+            		lwip.open(file.path, (err, img) => {
+            			if (!err)
+            				callback(null, {
+            					hash: crypto.createHmac('md5', `${contcfg.key}=${currentUser.id}`).update(file.path).digest('hex'),
+            					size: file.size,
+            					type: file.type,
+            					ext: file.type.substr(file.type.indexOf('/') + 1),
+            					owner_id: currentUser.id,
+            					assign_type: container,
+            					storage: contcfg.dirName
+            				}, img)
+            			else
+            				callback(err)
+            		})
+            	},
+            	(file, img, callback) => {
+            		// WRITE FILE
+    				img.writeFile(`./storage/${contcfg.dirName}/${file.hash}`, file.ext, {}, () => {
+            			// SAVE FILE
+	            		this.Model.create(file)
+	            			.then((media) => callback(null, media.uid))
+	            			.catch((err) => callback(err))
+    				});
+            	}
+            ], (err, result) => {
+            	// DONE
+            	if (!err)
+            		cb(null, {
+            			uid: result,
+            			message: 'image uploaded'
+            		})
+            	else
+            		cb(err)
+            });
+            
             return cb.promise
         })
     }
