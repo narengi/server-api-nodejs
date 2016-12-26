9cac2cd78aff88425d9e25d6df85bcb6629d6b31
Aref Mirhosseini <code@arefmirhosseini.com>
2016-12-26 17:15:21 +0330

    add post / route for handle newrelic error

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,6 +2,7 @@ module.exports = function(server) {
 	// Install a `/` route that returns server status
 	var router = server.loopback.Router();
 	router.get('/', server.loopback.status());
+	router.post('/', server.loopback.status());
 	server.use(router);
     server.setMaxListeners(128);
 };
